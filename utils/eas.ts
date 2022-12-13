f31d2ff817820861d6034ad4e5fb6f4cd10cd864
thankyouthatscool <85291297+thankyouthatscool@users.noreply.github.com>
2022-12-14 08:33:11 +1000

    123

---
+++ utils/eas.ts
@@ -0,0 +1,31 @@
+import { exec } from "child_process";
+import { promisify } from "util";
+
+const execPromise = promisify(exec);
+
+const SERVER_HOST = process.env.SERVER_HOST!;
+
+const updateSecrets = async () => {
+  const { stdout } = await execPromise("cd apps/inertiion && eas secret:list");
+
+  const lineArray = stdout.split("\n");
+
+  const nameLineIndex = lineArray.indexOf(
+    lineArray.find((line) => line.includes("CLI_TEST"))!
+  );
+
+  const idIndex = nameLineIndex - 1;
+  const idLine = lineArray[idIndex];
+  const secretId = idLine.slice(2).trim();
+
+  const { stdout: secretDeleteOutput } = await execPromise(
+    `cd apps/inertiion && eas secret:delete --id ${secretId} --non-interactive`
+  );
+  console.log(secretDeleteOutput);
+  const { stdout: create } = await execPromise(
+    `cd apps/inertiion && eas secret:create --scope project --name CLI_TEST --value ${SERVER_HOST} --type string --non-interactive`
+  );
+  console.log(create);
+};
+
+updateSecrets();
